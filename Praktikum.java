bc42260650b0ba3572179b473b6d4168f19057f0
Stanislav <stanislavshchekochikhin@yandex.ru>
2022-06-01 00:51:08 +0300

    Добавлены комментарии

--- Praktikum.java
+++ Praktikum.java
@@ -9,19 +9,19 @@ public class Praktikum {
 
         if (isLeapYear(year)) {
             // здесь нужно вывести результат
-            System.out.println("12.09." + year + ".");
+            System.out.println("12.09." + year + "."); //выводим результат для високосного года
         } else {
-            System.out.println("13.09." + year + ".");
+            System.out.println("13.09." + year + "."); //выводим результат для невисокосного года
         }
     }
 
     public static boolean isLeapYear(int year) {
         // здесь нужно определить, является ли переданный год високосным
-        if (year % 400 == 0) {
+        if (year % 400 == 0) {  //Проверяем, является ли год кратным 400
             return true ;
-        } else if (year % 100 == 0) {
+        } else if (year % 100 == 0) { //Проверяем, является ли год кратным 100
             return false;
-        } else if (year % 4 == 0) {
+        } else if (year % 4 == 0) { //Проверяем, является ли год кратным 4
             return true;
         } else {
             return false;
